546f9d9834b9fea7b19d62821cae5aefc3e52845
Sam Lachance <slachance@gmail.com>
2017-02-24 15:10:19 -0500

    integrated gitlab api. add most recent repos to front page. created git page to list all repos

--- app.rb
+++ app.rb
@@ -1,3 +1,25 @@
+require 'uri'
+require 'json'
+
 get '/' do
+  private_token = ENV["GITLAB_TOKEN"]
+
+  source = "https://gitlab.com/api/v3/groups/1356748/projects?private_token=#{private_token}"
+  uri = URI.parse(URI.encode(source))
+  api_response = Net::HTTP.get(uri)
+  @git_results = JSON.parse(api_response)
+
   slim :index
+end
+
+get '/git' do
+  private_token = ENV["GITLAB_TOKEN"]
+
+  source = "https://gitlab.com/api/v3/groups/1356748/projects?private_token=#{private_token}"
+  uri = URI.parse(URI.encode(source))
+  api_response = Net::HTTP.get(uri)
+  @results = JSON.parse(api_response)
+
+  slim :git
+
 end

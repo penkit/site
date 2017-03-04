f960d2001de18ef313eb0b1e2e49cbdf18a272d5
Sam Lachance <slachance@gmail.com>
2017-03-03 23:06:10 -0500

    implemented basic md guides

--- app.rb
+++ app.rb
@@ -2,6 +2,22 @@ get '/' do
   slim :index
 end
 
+get '/guides' do
+  slim :guides
+end
+
+get '/guides/:guide' do
+  guide = params[:guide]
+
+  begin
+    @example = File.open("#{File.dirname(__FILE__)}/guides/#{guide}.md", "rb").read
+  rescue
+    error 404
+  end
+  
+  slim :guide
+end
+
 not_found do
   status 404
   slim :oops
